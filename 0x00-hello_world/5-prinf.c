06e96392f64518b9d7beb6646dedc35ecb54670f
Loago <boloangloago@gmail.com>
2023-02-16 17:52:35 -0800

    5-prinf.c

---
+++ 0x00-hello_world/5-prinf.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main -a program that prin a line using printf function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper garmmer, but the outcome is a piece of art \n")
+	return (0);
+}
